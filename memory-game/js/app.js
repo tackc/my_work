8ab911661d0fe07ae0338c90b4b51cd68c9d7a73
Chris Tack <chris_tack@mac.com>
2018-09-24 13:50:16 -0700

    working on logic

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -1,9 +1,56 @@
-//Considering using vanilla javascript for my own practice
+/*----------------------------- constants -----------------------------*/
+const COLORS = ['cyan', 'magenta', 'yellow' 'gray']
 
-//Buttons (considering using hamburger menu)
-      //need 4 uniquely colored buttons for gameplay
-      //start game button
-      //counter that displays number of successful plays
+/*----------------------------- app's state (variables) -----------------------------*/
+var level;
+var computerMove = [];
+var playerMove = [];
+
+/*----------------------------- cached element references -----------------------------*/
+var cyan = document.getElementById('cyan');
+var magenta = document.getElementById('magenta');
+var yellow = document.getElementById('yellow');
+var gray = document.getElementById('gray');
+var startBtn = document.getElementById('start');
+
+/*----------------------------- event listeners -----------------------------*/
+cyan.addEventListener('click', );
+magenta.addEventListener('click', );
+yellow.addEventListener('click', );
+gray.addEventListener('click', );
+startBtn.addEventListener('click', );
+
+/*----------------------------- functions -----------------------------*/
+//Upon clicking "Start" button
+function computerMove() {
+      //Game randomly selects 1 of 4 buttons
+      
+      //Selected button id (1-4) is pushed into array & stored in var 
+                  //Number of items in array increases by 1 each round of play (max 50 items in array)
+            //Chosen button "lights up" and chosen sound is played
+            //Counter displays current level
+}
+
+function generateRandomColors() {
+      
+      computerMove = Math.floor(Math.random() * 5);
+      console.log(computerMove);
+}
+      
+
+
+
+
+//counter that displays number of successful plays
+
+
+
+
+
+
+
+//Buttons (hamburger menu)
+
       //button to bring the rules back up if desired
       
       //Possible Extras
@@ -12,12 +59,9 @@
 
 //Page loads with popup displaying rules of the game (considering using sweetalert2). This will allow the interface to be cleaner.
 
-//Upon clicking "Start" button
-      //Game randomly selects 1 of 4 buttons
-      //Selected button id (1-4) is pushed into array & stored in var 
-            //Number of items in array increases by 1 each round of play (max 100 items in array)
-      //Chosen button "lights up" and chosen sound is played
-      //Counter displays current level
+
+      
+      
 
 //Users turn to play
       //Click Button(s) (validated by comparing to random array) *** order needs to be the same
