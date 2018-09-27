226a11b1aad9d2be5a760897d3bede32ffeb01e0
Chris Tack <chris_tack@mac.com>
2018-09-27 13:42:49 -0700

    making progress with the logic

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -6,6 +6,7 @@ var level = "--";
 var computerMove = [];
 var playerMove = [];
 var turn;
+var computerIsPlaying = true;
 var computerMoveIndex;
 // var delay; 1000;
 
@@ -33,47 +34,97 @@ gray.addEventListener('click', playerClick);
 startBtn.addEventListener('click', startGame);
 resetBtn.addEventListener('click', resetGame);
 
+cyanSound.addEventListener('ended', soundEnded);
+magentaSound.addEventListener('ended', soundEnded);
+yellowSound.addEventListener('ended', soundEnded);
+graySound.addEventListener('ended', soundEnded);
 
 
 /*----------------------------- functions -----------------------------*/
 //reset game function
 function resetGame() {
       //reset level counter to 1
-      level = "--";
+      level = 0;
       computerMove = [];
       playerMove = [];
       startBtn.hidden = false;
       resetBtn.style.visibility = 'hidden';
-}    
+}   
+
+
+// Spins the CPU for a few milliseconds
+function sleep(milliseconds) {
+      var start = new Date().getTime();
+      for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+            break;
+            }
+      }
+}
+
+function lightButtonAndPlaySound(color) {
+      switch (color) {
+            case 'cyan':
+                  cyan.active = true;
+                  cyanSound.play().then(function() {
+                        console.log("playing cyan sound");
+                        cyan.active = false;
+                  });
+                  break;
+            case 'magenta':
+                  magenta.active = true;
+                  magentaSound.play().then(function() {
+                        console.log("playing magenta sound");
+                        magenta.active = false;  
+                  });
+                  break;
+            case 'yellow':
+                  yellow.active = true;
+                  yellowSound.play().then(function() {
+                        console.log("playing yellow sound");
+                        yellow.active = false;
+                  });
+                  break;
+            case 'gray':
+                  gray.active = true;
+                  graySound.play().then(function() {
+                        console.log("playing gray sound");
+                        gray.active = false;
+                  });
+                  break;
+      }
+      computerMoveIndex++;
+}
+
+// Sound ended listener function
+function soundEnded(evt) {
+      if (computerIsPlaying) {
+            lightButtonAndPlaySound(computerMove[computerMoveIndex]);
+            if (computerMoveIndex === turn) {
+                  computerIsPlaying = false;
+            }
+      }
+      computerMoveIndex = 0;
+}
+
+function computerPlay(turn) {
+      // Computer plays...
+      // console.log('computer is playing');
+      // computerIsPlaying = true;
+      lightButtonAndPlaySound(computerMove[0]);
+      // computerIsPlaying = false;
+      // console.log('computer has stopped playing');
+}
+
 function playGame() {
-      turn = 0;
+      // This initializes the new game
+      turn = 1;
       computerMoveIndex = 0;
       startBtn.hidden = true;
       resetBtn.style.visibility = 'visible';
-      
-      // while (player hasn't hit a bad button)
-      while (turn === computerMoveIndex) {
-            if (playerClick() === computerMove) {
-                  playerClick(computerMove);
-                  for (i=0; i < computerMove; i++) {
-                        console.log(computerMove);
-                        turn++;
-                        level++;
-                        computerMoveIndex++;
-                  }
-            } else {
-                  wrong.play();
-                  resetGame();
-            }
-      }
-      //enable ui interaction
-      //Human's turn
-            //User clicks {
-                  // validate clicks against computerMove
-                  //if correct...loop back to user clicks
-            // }
-            // else if user is wrong - tell the loop the user was wrong
-            
+      var error = false;
+
+      computerPlay(turn);     
 }
 
 //if player moves doesn't equal computer moves...you lose
@@ -85,20 +136,34 @@ function notEqual() {
 
 //user clicks stored in array
 function playerClick(evt) {
-      playerMove.push(evt.target.id);
-      playSound(evt);
+      // This is where we see if they clicked the right thing
+      if (evt.target.id === computerMove[computerMoveIndex]) {
+            // they clicked the right one
+            playSound(evt.target.id);
+            computerMoveIndex++;
+            if (computerMoveIndex === turn) {
+                  console.log("THIS SHOULD TRIGGER THE NEXT COMPUTER MOVE");
+                  computerMoveIndex = 0;
+                  turn++;
+                  sleep(500);
+                  computerPlay(turn);
+            }
+      } else {
+            // Oops!
+            wrong.play();
+      }
 }
 
 //play appropriate sound for each button on click
-function playSound(evt) {
+function playSound(color) {
       var sounds = {
             cyan: cyanSound, 
             magenta: magentaSound, 
             yellow: yellowSound, 
             gray: graySound
             };
-      sounds[evt.target.id].currentTime = 0;
-      sounds[evt.target.id].play();
+      sounds[color].currentTime = 0;
+      sounds[color].play();
 }
 
 //Upon clicking "Start" button
@@ -153,3 +218,24 @@ initialize();
       //Click Button(s) (validated by comparing to random array) *** order needs to be the same
             // If correct...level counter increases by 1
             // Else...game over message appears with button allowing user to play again
+
+
+// //helper function to compare playerMove array to computerMove array
+// function isEqual(arr1, arr2) {
+//       var arr1Len = arr1.length;
+//       var arr2Len = arr2.length;
+//       if (arr1Len !== arr2Len) return false;
+      
+//       function compare(item1, item2) {
+//             if (!isEqual(item1, item2)) return false;
+//             else {
+//                   if (item1 !== item2) return false;
+//             }
+//       } 
+      
+//       for (var i = 0; i < arr1Len; i++) {
+//             if (compare(arr1[i], arr2[i]) === false) return false;
+//       } 
+//       if (compare(arr1[key]), arr2[key] === false) return false;
+//       return true;
+// }
